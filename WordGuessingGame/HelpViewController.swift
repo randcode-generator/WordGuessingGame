4b723c8089a0c413682a13135ea6f9f26bbb1fe0
Eric Tang <tang8156@gmail.com>
2017-04-23 01:24:54 -0400

    add transition to help view controller

--- WordGuessingGame/HelpViewController.swift
+++ WordGuessingGame/HelpViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import MaterialMotion
 
 class HelpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        transitionController.transitionType = PushBackTransition.self
+        
+        let pan = UIPanGestureRecognizer()
+        transitionController.dismisser.dismissWhenGestureRecognizerBegins(pan)
+        view.addGestureRecognizer(pan)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,5 +28,39 @@ class HelpViewController: UIViewController {
     
     @IBAction func closeButtonAction(_ sender: AnyObject) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    private class PushBackTransition: Transition {
+        
+        required init() {}
+        
+        func willBeginTransition(withContext ctx: TransitionContext, runtime: MotionRuntime) -> [Stateful] {
+            let draggable = Draggable(withFirstGestureIn: ctx.gestureRecognizers)
+            
+            runtime.add(ChangeDirection(withVelocityOf: draggable.nextGestureRecognizer, whenNegative: .forward),
+                        to: ctx.direction)
+            
+            let bounds = ctx.containerView().bounds
+            let backPosition = CGPoint(x: bounds.midX, y: bounds.maxY + ctx.fore.view.bounds.height / 2)
+            let forePosition = CGPoint(x: bounds.midX, y: bounds.midY)
+            let movement = TransitionSpring(back: backPosition,
+                                            fore: forePosition,
+                                            direction: ctx.direction)
+            
+            let scale = runtime.get(ctx.back.view.layer).scale
+            
+            let tossable = Tossable(spring: movement, draggable: draggable)
+            
+            runtime.connect(runtime.get(ctx.fore.view.layer).position.y()
+                .rewriteRange(start: movement.backwardDestination.y,
+                              end: movement.forwardDestination.y,
+                              destinationStart: 1,
+                              destinationEnd: 0.95),
+                            to: scale)
+            
+            runtime.add(tossable, to: ctx.fore.view) { $0.xLocked(to: bounds.midX) }
+            
+            return [tossable]
+        }
     }
 }
